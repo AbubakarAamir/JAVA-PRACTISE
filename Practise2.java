826308b7c71137493f12b9d2da2bd17c76c92151
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-08-03 21:21:37 +0500

    Add files via upload

---
+++ Practise2.java
@@ -0,0 +1,85 @@
+
+class practise2 {
+    public static void main(String[] args) {
+
+        //here is the character methods:
+char mygrade = 'A';
+char var1 = 645, var2 = 646, var3 = 647;
+String greeting = "Assalam alikum ";
+System.out.println(greeting);
+
+System.out.println(mygrade);
+System.out.println(var1);
+System.out.println(var2);
+System.out.println(var3);
+
+  //HERE IS THE BOOLEAN METHODS :
+//Boolean:
+//1:TRUE/FALSE
+//2:ON/OFF 
+//3:YES/NO
+   
+
+  boolean java_is_fun = true;
+  boolean coding_is_false = false;
+
+  System.out.println(java_is_fun);
+  System.out.println(coding_is_false);
+
+        //Here is the integar types methods:
+//1: The byte data type can store whole numbers from -128 to 127
+        byte mynum = 21;
+        System.out.println(mynum);
+
+//2: The short data type can store whole numbers from -32768 to 32767:
+   short MYNUM = 1002;
+   System.out.println(MYNUM);
+   
+//3: The int data type can store whole numbers from -2147483648 to 2147483647.
+ int Mynum = 100000;
+ System.out.println(Mynum);
+ 
+//4: The long data type can store whole numbers from -9223372036854775808 to 9223372036854775807
+long MYNUm = 12345678912345345l;
+System.out.print(MYNUm);                                
+
+//5: Float is store minimum decimal point 
+float Decimal = 2.13f;
+System.out.println(Decimal);
+
+//6: Double is used to store more and large decimal points of digits 
+double more = 3.12345678d;
+System.out.println(more);
+
+// HERE IS THE STRINGS METHODS:
+        System.out.println("\nHERE IS THE TOP LANGUAGES IN CURRENT PHASE ");
+        String firstString = "java";
+        String secondString = "C/C++";
+        String thirdString = "python";
+        String fourthString = "javascript";
+        String fifthString = "react js ";
+        System.out.println(firstString); // print java here
+        System.out.println(secondString);// print c / c++
+        System.out.println(thirdString); // print python 
+        System.out.println(fourthString); // print javascript
+        System.out.println(fifthString); // print react js 
+
+         //How to find a lenght of a string .
+         String txt = "ABCDEFKLDCECREC";
+         System.out.println("THE LENGHT OF THE TEXT STRING IS " + txt.length());     
+
+
+//   HERE IS THE BASIC PROGRAMS RELATED TO THESE DATA TYPES :
+
+int  items = 50;
+float cost_per_item = 19.21f;
+float total_cost = items *cost_per_item;
+char currency = '$';
+
+System.out.println("Number of items " + items);
+System.out.println("Number of cost per items " +cost_per_item + currency);
+System.out.println("Total cost " + total_cost + currency);
+
+
+        }
+    }
