5bec204a42663e9918933bcfd00a0c41876b3fea
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-08-03 20:24:38 +0500

    Add files via upload

---
+++ Practise1.java
@@ -0,0 +1,38 @@
+//MADE BY :ABUBAKAR AAMIR
+//CLASS:SC-1
+//LANGUAGE:JAVA
+
+//   BASIC SYNTAX FOR JAVA PROGRAM 
+// class practise {
+//     public static void main(String[] args) {
+//         System.out.println("Hello ");
+//     }
+// }
+
+class second {
+    public static void main(String[] args) {
+        String NAME = "Abubakar";//String data type 
+        String FNAME = "Aamir"; //String data type
+        char Grade = 'B'; //Character data type
+        int AGE = 21; //integar data type 
+        float Height = 2.12f; //float data type 
+        boolean condition = true;// boolean data type 
+        System.err.println("HERE IS THE INFORMATION ON DIFFERENT DATA TYPES IN JAVA :");
+        System.out.println("YOUR NAME IS "+ NAME);
+        System.out.println("YOUR FATHER NAME IS"+ FNAME);
+        System.out.println("YOUR AGE IS " + AGE);
+        System.out.println( "YOUR GRADE IS " + Grade);
+        System.out.println("YOUR HEIGHT IS " + Height);
+        System.out.println("YOUR GIVEN CONDITION IS" + condition);
+
+       System.out.println("HERE IS THE BASIC CALCULATIONS:");//here we print the message by using system.out.println()
+       int a = 21;
+       int b = 30;
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println("THE SUM OF A+B = " + (a+b)); // Here is the addition process
+       System.out.println("THE PRODUCT OF A*B = " + (a*b)); // here is the product process
+       System.out.println("THE DIFFERENCE OF A-B = " + (a-b)); // here is the difference process
+       System.out.println("THE DIVISION OF A/B = "  + (a/b) );// here is the division process
+    }
+}
