ea01913b14ecf66f3e3336814e95fb56df9f78d5
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-08-06 21:09:04 +0500

    Add files via upload

---
+++ Practise5.java
@@ -0,0 +1,108 @@
+
+import javax.lang.model.util.ElementScanner14;
+
+//MADE BY :ABUBAKAR AAMIR.
+//CLASS: SC 1
+//LANGUAGE: JAVA
+
+//        IN THIS WE ARE DOING IF ELSE STATEMENTS AND WHILE AND DO-WHILE LOOP AND ALSO FOR LOOP
+
+
+public class Practise5 {
+    
+    public static void main(String[] args) {
+    
+        //HERE WE USE THE TERNARY OPERATOR :?
+
+    //     int time = 29;
+    //     String result = (time>10) ? "Good morning " : "Good night";
+    //    System.out.println(result);
+
+    //    int age = 11;
+    //    String finalage = (age<23) ? "Abhi bache ho aap " : "bht baree ho gai ho kiya ap";
+    //   System.out.println(finalage);
+ 
+
+        // int  age = 22;
+        // if(age>18){
+        //     System.out.println("yes you can vote fr the election");
+        // }
+
+        // else if (age<=18){
+        //     System.out.println("you cant vote because you are so young");
+        // }
+
+        // else {
+        //     System.out.println("wrong number");
+        // }
+
+
+
+    //     int time = 22; 
+         
+    //     if (time<11) {
+    //     System.out.println("Good morning sir ");
+    //     }
+    //   else if (time<15){
+    //     System.out.println("Good eveing sir ");
+    //   }
+
+    //   else if (time<24){
+    //     System.out.println("Good night sir ");
+    //   }
+
+    //   else {
+    //     System.out.println("You entered the wrong number ");
+    //   }
+
+
+
+        // int my_age = 13;
+        // int voting_age = 18;
+
+        // if (my_age>voting_age) {
+        //     System.out.println("Yes oofcourse you can vote for\n the election because your age is "+my_age );
+
+        // }
+        // else{
+        //     System.out.println("Sorry brother you can't vote for\n election because your age is" +voting_age);
+        // }
+
+       // RELATED PROBLEMS OF ELSE IF :
+//1:
+    //    int doorcode = 13353;
+    //    if (doorcode==13323) {
+    //     System.out.println("You entered the correct code.The door has been opened  ");
+    //    }
+
+    //    else{
+    //     System.out.println("You entered the wrong code . the door not opened for you");
+    //    }
+
+  //  2:
+//  int number = 20;
+//  if (number>0) {
+//     System.out.println("the nmumber is positive ");
+//  }
+
+// else if (number<0){
+//     System.out.println("the number  is negative");
+// }
+
+// else {
+// System.out.println("error");
+// }
+
+
+//3:
+
+// int mynum = 8;
+// if (mynum% 2== 0){
+//     System.out.println(mynum +"The number is even");
+// }
+// else{
+//     System.out.println(mynum +"The number is odd");
+// }
+
+    }
+}
