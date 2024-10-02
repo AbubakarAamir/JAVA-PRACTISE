50210ecfe5ddb780d8cbd0e7f32772140e47d864
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-10-03 00:33:14 +0500

    Add files via upload

---
+++ Practise11.java
@@ -0,0 +1,46 @@
+//Here we created program related to Encapsulation method of oops
+
+// Here we created the class name Practise11 because our file name is that
+// class Practise11 {
+//     // Here we have private data
+//     private String name;
+//     protected int age;
+
+//     // If we created private data, we have to use getter and setter methods to access it
+
+//     // Getter method for name (to access private data)
+//     public String getName() {
+//         return name;
+//     }
+
+//     // Setter method for name (to modify private data)
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     // Getter method for age (to access protected data)
+//     public int getAge() {
+//         return age;
+//     }
+
+//     // Setter method for age (to modify protected data)
+//     public void setAge(int age) {
+//         this.age = age;
+//     }
+// }
+
+// // Main class to run the program
+// public class Main { // Changed "main" to "Main"
+//     public static void main(String[] args) {
+//         // Here we create an object    
+//         Practise11 person = new Practise11();
+
+//         // Using setter methods to set data
+//         person.setName("Abubakar");
+//         person.setAge(21);
+
+//         // Using getter methods to access data 
+//         System.out.println("NAME: " + person.getName());
+//         System.out.println("AGE:  " + person.getAge());
+//     }
+// }
